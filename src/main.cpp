3e56605683a68bec57cadc4b740e46f7731c2fa0
senthilpoo10 <senthilpoo10@gmail.com>
2025-04-06 09:27:59 +0300

    added isValidArgs() in main()

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "../include/Server.hpp"
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-	
+int isValidArgs(char **av) {
+    std::string port = av[0];
+    std::string password = av[1];
+
+    if (port.empty() || password.empty())
+        return 1;
+    for (size_t i = 0; i < port.length(); i++) {
+        if (!std::isdigit(port[i]))
+            return 1;
+    }
+    for (size_t i = 0; i < password.length(); i++) {
+        if (std::isspace(password[i]))
+            return 1;
+    }
+    int portNum = std::atoi(av[0]);
+    if (portNum == 0 || portNum <= 1023)
+        return 1;
+    else if (portNum >= 65535)
+        return 1;
     return 0;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3 || isValidArgs(&argv[1])){
+        std::cout << "USAGE : " << RED << "./irc_serv [port] [password]" << RESET << std::endl;
+        return 1;
+    }
+    Server server(argv[1], argv[2]);
+    server.startServer();
 }
